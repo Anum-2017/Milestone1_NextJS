7f04c1472dd20677be40a72815343453552b61c4
Anum-2017 <anumriz2017@gmail.com>
2024-08-10 23:49:37 +0500

    Next JS

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <div className=" pt-24 flex flex-col justify-center items-center">
+    <h1 className="text-center  text-4xl font-semibold">1st Milestone Project Of Next.js</h1>
+  <div className=" bg-blue-600 mt-7 h-72 w-80 rounded-xl flex justify-center items-center ">
+  <h1 className="text-center font-semibold   text-white text-4xl">Hello World !!</h1> 
+  </div>
+</div>
+  );
+}
